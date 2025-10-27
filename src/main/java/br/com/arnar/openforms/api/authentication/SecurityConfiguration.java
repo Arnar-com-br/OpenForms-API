734fb48e774a796dc53b87a93fc789dc405ffa3d
Arthur Araujo <arthur.araujo@tutanota.com>
2025-10-27 14:31:37 -0300

    Configure cors in filterChain

--- src/main/java/br/com/arnar/openforms/api/authentication/SecurityConfiguration.java
+++ src/main/java/br/com/arnar/openforms/api/authentication/SecurityConfiguration.java
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+
+        http.cors(Customizer.withDefaults());
 
         http.sessionManagement(management -> management
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
